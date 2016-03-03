f68276137810c4f9e1c5b649f764a62a79bb0bce
turquoise <monica.gosschalk@btinternet.com>
2016-03-03 15:10:03 +0000

    Changed Pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-	attr_accessor :image_file_name
+	attr_accessor :image
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
