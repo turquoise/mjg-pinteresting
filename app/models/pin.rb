9b1669aab252617184c9de382ac8ceae113381d6
turquoise <monica.gosschalk@btinternet.com>
2016-03-03 15:15:22 +0000

    Added validates_attachment

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,9 @@
 class Pin < ActiveRecord::Base
-	attr_accessor :image
+
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	validates :image, presence: true
 	validates :description, presence: true
 end
